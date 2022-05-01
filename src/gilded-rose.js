edd273f8800454c2989687d8cb9b632106628a14
tealyao <tealyao@tencent.com>
2022-05-01 17:37:00 +0800

    if complete done

--- src/gilded-rose.js
+++ src/gilded-rose.js
@@ -37,22 +37,14 @@ export class GildedRose {
             item.quality = item.quality - item.quality
           }
         } else {
-          if (item.quality > 0) {
-            if (item.name !== 'Sulfuras, Hand of Ragnaros') {
+          if (item.name === 'Sulfuras, Hand of Ragnaros') {
+            // do nothing
+          } else {
+            if (item.quality > 0) {
               item.quality = item.quality - 1
             }
-          }
-          if (item.name !== 'Sulfuras, Hand of Ragnaros') {
             item.sell_in = item.sell_in - 1
-          }
-          if (item.sell_in < 0) {
-            if (item.name !== 'Backstage passes to a TAFKAL80ETC concert') {
-              if (item.quality > 0) {
-                if (item.name !== 'Sulfuras, Hand of Ragnaros') {
-                  item.quality = item.quality - 1
-                }
-              }
-            } else {
+            if (item.sell_in < 0) {
               item.quality = item.quality - item.quality
             }
           }
